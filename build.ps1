7dad22dd78541860a17129d472673cda731ba311
Feodor Fitsner <feodor@appveyor.com>
2017-03-16 13:09:37 -0700

    Building Rx.NET only [skip ci]

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ $repositoryRoot = split-path $MyInvocation.MyCommand.Definition
 Write-Host "Building Rx.NET" -ForegroundColor Green
 .\Rx.NET\Source\build-new
 
+<#
 Write-Host "Building Ix.NET" -ForegroundColor Green
 .\Ix.NET\Source\build-new
 
@@ -10,3 +11,4 @@ if ($env:CI -ne 'True') {
   Write-Host "Reverting AssemblyInfo's" -Foreground Green
   gci $scriptPath -re -in AssemblyInfo.cs | %{ git checkout $_ } 
 }
+#>
